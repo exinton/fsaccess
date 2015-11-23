501530fecbe9654db3065e452917cc0316b5ed68
xin tong <exinton@gmail.com>
2015-11-23 15:16:03 -0600

    added initfs
    added initfs

--- newfile.c
+++ newfile.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <fcntl.h>
 #include <math.h>
+#include <time.h>
+#include <signal.h>
 
 /*
   Function Declarations for builtin shell commands:
@@ -13,6 +15,7 @@
 int sh_test(char **);
 int sh_exit(char **);
 int sh_initfs(char **);
+int static initblock[4]={0,0,0,0};
 
 struct superblock {
 	unsigned short isize[1];
@@ -28,6 +31,31 @@ struct superblock {
 	};
 
 
+struct inodeFlag{
+	unsigned int alloc:1;
+	  unsigned int fileType:2;
+	  unsigned int largeFile:1;
+	  unsigned int setUid:1;
+	  unsigned int setGrp:1;
+	  unsigned int unalloc:1;
+	  unsigned int readOwner:1;
+	  unsigned int writeOwner:1;
+	  unsigned int exeOwner:1;
+	  unsigned int wregrp:3;
+	  unsigned int wreothers:3;
+}park;
+
+struct inode{  //inode's length is 32 in total
+	struct inodeFlag flag;
+	char nlinks[1];		//number of links for files
+	char uid[1];
+	char gid[1];
+	char size0[1];
+	unsigned short size1[1];
+	unsigned short address[8][1];	//address of data blocks pointing to
+	unsigned short acttime[2][1];	//time of last access
+	unsigned short modtime[2][1];	//time of last modification
+};
 
 
 
@@ -76,96 +104,437 @@ int sh_test(char **args)
 }
 
 //calculate the nfree size
-int nfreeblk(int n2 , int n1){
- int z = nisize(n2);
- int x = (n1-2-z)/101;
- int r = (n1-2-z)%101;
- if (r = 0)
-   { 
-   return x;
-   } 
- else
-   {
-   return x+1;
-   }
-}
+
+
 
 //caculate the isize
 
 int nisize(int n){
 
-return  ((n-1)/16)+2;  //works as ceil or roundup
+	if (n%16 == 0){
+		return n/16;
+	}
+	else{
+		return n/16+1;
+	}
+}
 
+int nfreeblk(int n1, int n2){
+
+ int z ;
+ z = nisize(n2);
+
+ if ( n1-2-z >100 )
+ {
+	 return 100;
+ }
+ else
+ {
+	 return n1-2-z;
+ }
+
+ }
+
+
+
+void add_block(int fd,int blockToCheck, int offset, int blockToFree){  //add the blocknumber into free block array, and return the free blocks in the array
+//where does the blocknumber comes from when initializing
+//they come from the total free blocks
+
+	//buffer to store the content in nfree and free[]
+	unsigned short buffer[101];
+	//go to the nfree of the super block
+	int position=lseek(fd,512*blockToCheck+offset,SEEK_SET);//
+	//read the nfree and free[] array fields
+	int numberReaded =read (fd,buffer,202);
+
+
+	if ( (buffer[0] < 100) && (buffer[1] == 0)){ //if only superblock has list, free[0] =0
+	//add the free blocks into free block list in super block
+		buffer[0]=buffer[0]+1;
+		buffer[buffer[0]+1]=blockToFree;
+		lseek(fd,512*blockToCheck+offset,SEEK_SET);
+
+		int numberWrited=write(fd,buffer,202);
+
+	}
+
+	else if ((buffer[0] =100) && (buffer[1] == 0)){
+    //fill the free[0] with the blockToFree's number as the head of next chain
+		buffer[1]=blockToFree;
+		lseek(fd,512*blockToCheck+offset,SEEK_SET);
+		int numberWrited=write(fd,buffer,202);
+		lseek(fd,512*blockToFree,SEEK_SET);
+		numberWrited=write(fd,initblock,4);
+		printf("case 2 buffer[0], buffer[1] is %d %d !\n",buffer[0],buffer[1]);
+		sleep(1);
+	}
+
+	else if (buffer[1] != 0){
+
+		add_block(fd,buffer[1],0,blockToFree);
+
+	}
+
+}
+
+
+int get_block(int fd){
+	int blockToCheck=1;
+	int offset =4;
+
+	unsigned short buffer[101];
+		//go to the nfree of the super block
+
+	int position=lseek(fd,512*blockToCheck+offset,SEEK_SET);//
+		//read the nfree and free[] array fields
+	int numberReaded =read (fd,buffer,202);
+
+	if (buffer[0]==0 && buffer[1]==0){ // no free data blocks in SB and DB
+
+		return 0;
+	}
+	else if (buffer[0]>0) {
+		printf("case 2:before get buffer[0], buffer[1] is %d %d !\n",buffer[0],buffer[1]);
+		buffer[0]=buffer[0]-1;
+		printf("case 2: after get buffer[0], buffer[1] is %d %d !\n",buffer[0],buffer[1]);
+		printf("case 2: return value  %d !\n",buffer[buffer[0]-1]);
+		int position=lseek(fd,512*blockToCheck+offset,SEEK_SET);//
+		int numberWrited=write(fd,buffer,202);
+
+
+		return buffer[buffer[0]-1];
+	}
+	else if (buffer[0]==0 && buffer[1]!=0){
+		printf(" case 3: before get buffer[0], buffer[1] is %d %d !\n",buffer[0],buffer[1]);
+		lseek(fd,512*buffer[1],SEEK_SET);
+		int temp=buffer[1];
+		int numberReaded =read (fd,buffer,202);
+		lseek(fd,512*blockToCheck+offset,SEEK_SET);
+		int numberWrited=write(fd,buffer,202);
+		printf("case 3: after get buffer[0], buffer[1] is %d %d !\n",buffer[0],buffer[1]);
+		return temp;
+	}
+
+
+}
+
+
+//add mutex for inode in the future
+
+
+void free_inode(int fd, int inodeNumber){
+//set the inode valib bit =1;
+
+	  if (inodeNumber <2 ){
+
+		  printf("can not free inode less than 2! or larger than defined\n");
+		  return ;
+
+		  }
+
+//get the inode offset position
+
+	  lseek(fd,2*512+(inodeNumber-1)*32,SEEK_SET);
+	  struct inodeFlag inodeFlag0,*ptr_inodeFlag;
+	  int numberReaded =read (fd,ptr_inodeFlag,2);
+	  printf("before get inode flag alloc %d !\n",inodeFlag0.alloc);
+	  inodeFlag0.alloc=0;
+
+
+	  lseek(fd,2*512+(inodeNumber-1)*32,SEEK_SET);
+	  int numberWrited=write(fd,ptr_inodeFlag,2);
+	  lseek(fd,2*512+(inodeNumber-1)*32,SEEK_SET);
+	  numberReaded =read (fd,ptr_inodeFlag,2);
+	  printf("after get inode alloc flag 1 %d !\n",inodeFlag0.alloc);
+
+	  //get the ninode
+	  lseek(fd,1*512+206,SEEK_SET);
+	  unsigned short buffer[1];
+	  numberReaded =read (fd,buffer,2);
+
+
+		//update the ninode and inode array
+
+	  lseek(fd,1*512+208+buffer[0],SEEK_SET);
+	  numberWrited=write(fd,&inodeNumber,2);
+
+		 lseek(fd,1*512+206,SEEK_SET);
+		 buffer[0]=buffer[0]+1;
+		 numberWrited=write(fd,buffer,2);
+}
+
+
+int scan_inodeList(int fd){
+
+	unsigned short bufferInSB[101];
+
+	//check the inodelist
+	lseek(fd,1*512+206,SEEK_SET);
+	int numberReaded =read (fd,bufferInSB,2);
+
+   //get isize
+	unsigned short buffer[1];
+	lseek(fd,1*512,SEEK_SET);
+	numberReaded =read (fd,buffer,2);
+	int nisize = buffer[0];
+	//scan the blocks of inode list
+	lseek(fd,2*512+32,SEEK_SET);
+	int inodeToScan=nisize*512-32;
+	int validInodeScanned;
+	int inodeNumber=2;  //root inode is excluded
+	while (inodeToScan>0 && bufferInSB <=100){
+
+
+		 struct inodeFlag inodeFlag0,*ptr_inodeFlag;
+		 int numberReaded =read (fd,ptr_inodeFlag,2);
+
+		 //check validness
+		 if (inodeFlag0.alloc==0){
+			 free_inode(fd,inodeNumber);
+			 validInodeScanned=validInodeScanned+1;
+		 }
+
+
+		 inodeNumber=inodeNumber+1;
+	}
+	return validInodeScanned;
+
+
+}
+
+int get_inode(int fd){
+
+	unsigned short buffer[101];
+
+	//check the inodelist
+	lseek(fd,1*512+206,SEEK_SET);
+	int numberReaded =read (fd,buffer,2);
+	if (buffer[0]==0){
+
+
+		if(scan_inodeList(fd) >0 ){
+
+			get_inode(fd);
+		}
+		else {
+
+			return 0;
+		}
+	}
+
+	else if(buffer[0]>0){
+
+		//update the ninode;
+		lseek(fd,1*512+206,SEEK_SET);
+		int temp[1];
+		numberReaded =read (fd,temp,2);
+		int inodeNumber=temp[0]; //inodenumber mush larget than 1 to exclude root dirctory
+		temp[0]=temp[0]-1;
+		int numberWrited=write(fd,temp,2);
+
+
+		//get the inode offset position
+		lseek(fd,2*512+(inodeNumber-1)*32,SEEK_SET);
+		struct inodeFlag inodeFlag0,*ptr_inodeFlag;
+		numberReaded =read (fd,ptr_inodeFlag,2);
+		inodeFlag0.alloc=1;
+		lseek(fd,2*512+(inodeNumber-1)*32,SEEK_SET);
+		numberWrited=write(fd,ptr_inodeFlag,2);
+
+
+
+		return inodeNumber;
+	}
+
+
+
+	//return inode number
+	return 1;
+}
+
+
+
+void init_inode(int fd, int *inodeInputArray){
+	//
+	struct inodeFlag inodeFlag0,*ptr_inodeFlag;
+	struct inode inode0, *ptr_inode;
+	ptr_inode=&inode0;
+	inode0.flag.alloc=1;
+
+
+}
+
+//void init_iList(int fd,int numOfInode){
+//	int i;
+//	for (i=1;i<=numOfInode;i=i+1){
+//		//initial inode from 1 to numOfInode
+//		init_inode(fd,i);
+//	}
+//
+//}
+
+void clear_block(int fd, int blockNumber){
+	lseek(fd,512*blockNumber,SEEK_SET);
+	int count =512;
+	int value[1];
+	value[0]=0;
+	while(count>0){
+
+		int numberWrited=write(fd,value[0],1);
+		count=count-1;
+
+	}
+
+
+}
+
+void init_root_directory(int fd){
+
+	//get inode
+	lseek(fd,512*2+32*(0)+8,SEEK_SET);
+
+
+	//init_inode(fd,directory);
+	int blockNumber=get_block(fd);
+
+	int address[2][1];
+	address[0][0]=blockNumber;
+	address[1][0]=1;
+
+	int numberWrited=write(fd,address[0][0],2);
+
+	//manipulate the data blocks
+
+	clear_block(fd,blockNumber);
+	printf(" hanging inside init_root! really \n");
+	lseek(fd,512*blockNumber,SEEK_SET);
+	char pathName[2][1];
+	pathName[0][0]=".";
+	pathName[1][0] ="..";
+	numberWrited=write(fd,address[1][0],2);
+	numberWrited=write(fd,pathName[0][0],14);
+
+	numberWrited=write(fd,address[1][0],2);
+	numberWrited=write(fd,pathName[1][0],14);
+
+
+}
+
+
+
+
+
+void init_directoryDataBlock(int df,int *dirDBArray){
+
+}
+
+
+
+void init_bootBlock(){
+//do nothing
 }
 
 
 
 int sh_initfs(char **args)
 {
-
-  struct superblock sb,*ptr_sb;
-  ptr_sb=&sb; 
- 
-  //trouble shooting print out
+   //trouble shooting print out
   printf("fsaccess\n");
   printf( "You entered: %s %s %s %s \n ", args[0],args[1], args[2],args[3]);
   int n1 = atoi(args[2]);
   int n2 = atoi(args[3]);
-  //used variables   
-    
+  int freeblocks = n1-2-nisize(n2);		//total number of free datablocks in the volume
+  //used variables
 
-  sb.isize[0] = (unsigned short) nisize(n2);
-  sb.fsize[0] = (unsigned short) 0;
-  sb.nfree[0] = (unsigned short) nfreeblk(n2,n1);
-  
 
-  int i = 0;
-    for (i;i<100;i=i+1)
-    {
-     sb.free[i][0] =(unsigned short) (i+102); 
-    }
 
-  int LENGTHOFBOOTBLOCK=512;
-  
- sb.ninode[0] =(unsigned short) 100;
-  
-  int j = 0;
-    for (j;j<100;j=j+1)
-    {
-     sb.inode[j][0]= (unsigned short)(i+2);
-    }
 
-  
-//check point
-  printf("isize is %d \n",nisize(n2));
-  
   //open the file as partition
-  int fd=open(args[1],O_RDWR | O_CREAT,0666);
- 
-  //offset 512 byte to skip boot blocks 
-   lseek(fd,LENGTHOFBOOTBLOCK,SEEK_SET);
+int fd=open(args[1],O_RDWR | O_CREAT,0666);
 
-  //write the isize to super block
-    write(fd,ptr_sb->isize,2);
 
-  //write the fsize to super block
-  write(fd,ptr_sb->fsize,2);
+//init super block
+struct superblock sb,*ptr_sb;
+	  ptr_sb=&sb;
+	 freeblocks = n1-2-nisize(n2);		//total number of free datablocks in the volume
+	   //used variables
+	  sb.isize[0] = (unsigned short) nisize(n2); //number of blocks devoted to the inode-list
+	   sb.fsize[0] = (unsigned short) n1;//first block not used
+	   sb.nfree[0] = (unsigned short) 0; // initial to 0
 
-  //write the free, which is the number of free blocks
-  write(fd,ptr_sb->nfree,2);
 
-  //write the free block array into free[]
-  write(fd,ptr_sb->free,200);
+	   int i;
+	   for (i=0;i<100;i=i+1){
+	   	//printf("add block %d \n",i);
+	   	 sb.free[i][0]=0;
+	   }
 
-  //write the free block array into ninode[]
-  write(fd,ptr_sb->ninode,2);
+	   if (n2 > 100){
+		   sb.ninode[0] =(unsigned short) 100 ;
+	   }
+	   else
+	   {
+	   sb.ninode[0] =(unsigned short) n2 ;
+	   }
 
-  //write inode into super block
-  write(fd,ptr_sb->inode,200);
+	   int j;
+
+	       for (j=0;j<100;j=j+1)
+	       {
+	        sb.inode[j][0]= (unsigned short)(j+1);
+	       }
+
+	   //flock, ilock
+	       sb.flock[0]=0; //tbd
+	       sb.ilock[0]=0; //tbd
+	       sb.fmod[0]=0;
+	       time_t tloc;
+	       tloc=time(NULL);
+	       sb.time[0]=(unsigned short) tloc;
+
+	       lseek(fd,1*512,SEEK_SET);
+
+	       int numberWrited=write(fd,ptr_sb,415);
+
+
+
+
+
+  printf(" test hanging in init! \n");
+
+
+
+
+//add blocks
+
+  for (i=0;i<freeblocks;i=i+1){
+ 	   	//printf("add block %d \n",i);
+	  add_block(fd, 1, 4, 2+nisize(n2)+i );
+
+ 	   }
+
+
+
+
+
+
+//initial inode for root directory
+  init_root_directory(fd);
+
+
+
+
+
+  //  init_inode(fd, inodeDirInput);
+
+
+  printf(" hanging here! \n");
+
 
 
   close(fd);
- 
+
  return 1;
 }
 
@@ -240,11 +609,11 @@ char *sh_read_line(void)
 {
   int position = 0;
   int c;
-  char *buffer = malloc(sizeof(char) * 1024);
+  char *buffer = (char*)malloc(sizeof(char) * 1024);
 
   while (1) {
     // Read a character
-    c = getchar(); 
+    c = getchar();
     // If we hit EOF, replace it with a null character and return.
     if (c == EOF || c == '\n') {
       buffer[position] = '\0';
@@ -273,7 +642,7 @@ char **sh_split_line(char *line)
   token = strtok(line, SH_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
-    position++;  
+    position++;
     token = strtok(NULL, SH_TOK_DELIM);
   }
   tokens[position] = NULL;
@@ -320,6 +689,7 @@ int main(int argc, char **argv)
 
   return EXIT_SUCCESS;
 }
+
 
 
 
